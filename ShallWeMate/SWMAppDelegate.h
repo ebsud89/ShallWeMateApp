e42282cd208063f732590633984fb50d2bb4d5a5
Kim Junhyoung <ebsud89@gmail.com>
2014-07-31 15:14:11 +0900

    Initial Commit

---
+++ ShallWeMate/SWMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SWMAppDelegate.h
+//  ShallWeMate
+//
+//  Created by Kim Junhyoung on 7/31/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShallWeMate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShallWeMate
+//
+//  Created by Kim Junhyoung on 7/31/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SWMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SWMAppDelegate class]));
+    }
+}
